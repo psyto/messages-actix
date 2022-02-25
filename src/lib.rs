de71ecc28f5abeeb12a9c77c478cdb895e9337be
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-02-25 13:40:22 +0900

    minor change

--- src/lib.rs
+++ src/lib.rs
@@ -30,7 +30,7 @@ impl MessageApp {
                 .service(echo)
                 .route("/hey", web::get().to(manual_hello))
         })
-        .bind("127.0.0.1:8080")?
+        .bind(("127.0.0.1", self.port))?
         .run()
         .await
     }
